031e820ead26741ef72bbb88bb6bb6c5ad20e871
Daan Klarenbeek <github@ijskoud.dev>
2023-04-18 20:55:54 +0200

    fix(GitCordClient): incorrectly initializing of client

--- src/lib/GitCordClient.ts
+++ src/lib/GitCordClient.ts
@@ -1,17 +1,18 @@
 import { IgloClient } from "@snowcrystals/iglo";
-import { BOT_COMMANDS_DIR, BOT_LISTENER_DIR } from "./constants.js";
+import { BOT_COMMANDS_DIR, BOT_INTERACTIONS_DIR, BOT_LISTENER_DIR } from "./constants.js";
 
 export default class GitCordClient extends IgloClient {
 	public constructor() {
 		super({
 			client: { intents: ["GuildWebhooks", "Guilds"], allowedMentions: { repliedUser: true, roles: [], users: [] } },
-			paths: { commands: BOT_COMMANDS_DIR, events: BOT_LISTENER_DIR, interactions: BOT_LISTENER_DIR },
+			paths: { commands: BOT_COMMANDS_DIR, events: BOT_LISTENER_DIR, interactions: BOT_INTERACTIONS_DIR },
 			logger: { defaultFormat: { infix: "[BOT]" } }
 		});
 	}
 
 	/** Starts the Discord bot and it's processes */
 	public start() {
-		void this.login(process.env.DISCORD_BOT_TOKEN);
+		void this.run(process.env.DISCORD_BOT_TOKEN);
+		console.log(this.listenerCount("ready"));
 	}
 }
