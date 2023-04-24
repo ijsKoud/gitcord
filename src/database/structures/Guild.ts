cada1ceb33ca728e2b615a7be89577cd6789008c
Daan Klarenbeek <github@ijskoud.dev>
2023-04-24 14:52:15 +0200

    feat: add commands (#8)
    * feat: add webhooks list command
    
    * feat: add create webhook command
    
    * feat: add delete command
    
    * feat: add get secret command

--- src/database/structures/Guild.ts
+++ src/database/structures/Guild.ts
@@ -1,7 +1,8 @@
 import type { GuildConfig } from "#database/types.js";
 import type GitCordClient from "#discord/lib/GitCordClient.js";
-import { Collection, Guild } from "discord.js";
+import { ChannelType, Collection, ForumChannel, Guild, TextChannel } from "discord.js";
 import GitCordGuildWebhook from "./GuildWebhook.js";
+import { randomBytes } from "node:crypto";
 
 export default class GitCordGuild {
 	public guild!: Guild;
@@ -26,6 +27,46 @@ export default class GitCordGuild {
 			this.webhooks.set(gitcordWebhook.id, gitcordWebhook);
 		}
 
+		return true;
+	}
+
+	/**
+	 * Creates a new webhook for GitHub notifications
+	 * @param channel The channel the notifications should be posted in
+	 * @throws Error with reason why the creation failed
+	 */
+	public async create(channel: ForumChannel | TextChannel) {
+		const type = channel.type === ChannelType.GuildForum ? "FORUM" : "CHANNEL";
+		const webhook = await channel.createWebhook({ name: "GitCord", avatar: "https://cdn.ijskoud.dev/files/2zVGPBN3ZmId.webp" }).catch(() => {
+			throw new Error("Unable to create a webhook, probably missing permissions.");
+		});
+
+		const secret = randomBytes(64).toString("hex");
+
+		const prismaData = await this.client.prisma.guildWebhook.create({
+			data: { type, webhookUrl: webhook.url, guildId: this.guildId, webhookId: webhook.channelId, webhookSecret: secret }
+		});
+
+		const gitcordWebhook = new GitCordGuildWebhook(this, prismaData);
+		this.webhooks.set(webhook.channelId, gitcordWebhook);
+
+		return gitcordWebhook;
+	}
+
+	/**
+	 * Deletes an existing webhook
+	 * @param webhookId The webhook to delete
+	 * @returns Boolean depending on the existence of the webhook
+	 * @throws DiscordApiError if the webhook deletion fails
+	 */
+	public async delete(webhookId: string) {
+		const webhook = this.webhooks.get(webhookId);
+		if (!webhook) return false;
+
+		await webhook.discordWebhook.delete();
+		await this.client.prisma.guildWebhook.delete({ where: { webhookId } });
+
+		this.webhooks.delete(webhookId);
 		return true;
 	}
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "node:url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // === Discord bot directory constants ===
-export const BASE_BOT_DIR = join(__dirname, "..", "bot");
+export const BASE_BOT_DIR = join(__dirname, "..", "discord", "bot");
 export const BOT_COMMANDS_DIR = join(BASE_BOT_DIR, "commands");
 export const BOT_LISTENER_DIR = join(BASE_BOT_DIR, "listeners");
 export const BOT_INTERACTIONS_DIR = join(BASE_BOT_DIR, "interactions");
