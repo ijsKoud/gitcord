808545f6d3b90e9787e86e9f4da8721ff146c14b
Daan Klarenbeek <github@ijskoud.dev>
2023-04-24 18:19:22 +0200

    feat(PullRequestEmbed): add missing types

--- src/github/embeds/pr/PullRequestEmbed.ts
+++ src/github/embeds/pr/PullRequestEmbed.ts
@@ -1,8 +1,20 @@
 import { GitHubEmbed, type GitHubEmbedOptions } from "#github/lib/embed/structures/GitHubEmbed.js";
-import type { PullRequestClosedEvent, PullRequestEvent, PullRequestOpenedEvent, PullRequestReopenedEvent } from "@octokit/webhooks-types";
+import type {
+	PullRequestAssignedEvent,
+	PullRequestClosedEvent,
+	PullRequestDemilestonedEvent,
+	PullRequestEvent,
+	PullRequestMilestonedEvent,
+	PullRequestOpenedEvent,
+	PullRequestReopenedEvent,
+	PullRequestReviewRequestRemovedEvent,
+	PullRequestReviewRequestedEvent,
+	PullRequestUnassignedEvent
+} from "@octokit/webhooks-types";
 import type { EmbedBuilder } from "discord.js";
 import { ApplyOptions } from "#github/lib/embed/decorators.js";
 import { EMBED_COLORS } from "#github/lib/types.js";
+import _ from "lodash";
 
 @ApplyOptions<GitHubEmbedOptions>({ name: "pull_request" })
 export default class extends GitHubEmbed {
@@ -22,6 +34,18 @@ export default class extends GitHubEmbed {
 			case "locked":
 			case "unlocked":
 				this.stageChange(event, embed);
+				break;
+			case "assigned":
+			case "unassigned":
+				this.assignUpdate(event, embed);
+				break;
+			case "demilestoned":
+			case "milestoned":
+				this.milestoneUpdate(event, embed);
+				break;
+			case "review_requested":
+			case "review_request_removed":
+				this.reviewUpdate(event, embed);
 				break;
 			case "auto_merge_disabled":
 			case "auto_merge_enabled":
@@ -58,7 +82,49 @@ export default class extends GitHubEmbed {
 
 		embed
 			.setColor(EMBED_COLORS.UPDATE)
-			.setTitle(`${event.repository.full_name} — ${event.pull_request.title}`)
-			.setDescription(`State: **${state}**`);
+			.setTitle(`${event.repository.full_name} — Pull Request Stage Update`)
+			.setDescription(`**${event.pull_request.title}**\nState: **${state}**`);
+	}
+
+	private assignUpdate(event: PullRequestAssignedEvent | PullRequestUnassignedEvent, embed: EmbedBuilder) {
+		embed
+			.setTitle(`${event.repository.full_name} — Pull Request User ${_.capitalize(event.action)}`)
+			.setDescription(
+				[
+					`**${event.pull_request.title}**`,
+					`Action: \`${_.capitalize(event.action)}\``,
+					`Assignee: [${event.assignee.login}](${event.assignee.html_url})`
+				].join("\n")
+			);
+	}
+
+	private milestoneUpdate(event: PullRequestMilestonedEvent | PullRequestDemilestonedEvent, embed: EmbedBuilder) {
+		embed.setDescription(
+			[
+				`**${event.pull_request.title}**`,
+				`Action: \`${_.capitalize(event.action)}\``,
+				`Milestone: [${event.milestone.title}](${event.milestone.html_url})`
+			].join("\n")
+		);
+	}
+
+	private reviewUpdate(event: PullRequestReviewRequestedEvent | PullRequestReviewRequestRemovedEvent, embed: EmbedBuilder) {
+		if ("requested_reviewer" in event) {
+			const action = event.action
+				.replace(/\_/g, " ")
+				.trim()
+				.toLowerCase()
+				.split(" ")
+				.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+				.join(" ");
+
+			embed.setDescription(
+				[
+					`**${event.pull_request.title}**`,
+					`Action: \`${action}\``,
+					`Reviewer: [${event.requested_reviewer.login}](${event.requested_reviewer.html_url})`
+				].join("\n")
+			);
+		}
 	}
 }
