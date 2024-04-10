5414c507155b6fad06f91e96c01fab5f55f7f00b
Daan Klarenbeek <github@ijskoud.dev>
2024-04-10 13:17:12 +0200

    feat(Database): add seeding option

---
+++ packages/database/src/lib/seed.ts
@@ -0,0 +1,20 @@
+import { GuildModel, GuildWebhookModel } from "index.js";
+
+import { env } from "@/shared/env.js";
+
+async function seed() {
+	if (!env.DEVELOPMENT_GUILD_ID || !env.DEVELOPMENT_WEBHOOK_ID || !env.DEVELOPMENT_WEBHOOK_URL)
+		throw new Error("Missing development environment variables");
+
+	const guildModel = new GuildModel();
+	await guildModel.create([{ id: env.DEVELOPMENT_GUILD_ID }]);
+
+	const guildWebhookModel = new GuildWebhookModel();
+	await guildWebhookModel.create([
+		{ guildId: env.DEVELOPMENT_GUILD_ID, id: env.DEVELOPMENT_WEBHOOK_ID, webhook: env.DEVELOPMENT_WEBHOOK_URL, type: "text" }
+	]);
+
+	console.info("Seeded completed successfully");
+}
+
+await seed();
