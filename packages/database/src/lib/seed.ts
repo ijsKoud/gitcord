7a5c863e043638fe6330c2244cc9519535d38324
Daan Klarenbeek <github@ijskoud.dev>
2024-04-14 13:06:34 +0200

    fix: secret missing in seeding

--- packages/database/src/lib/seed.ts
+++ packages/database/src/lib/seed.ts
@@ -3,7 +3,7 @@ import { GuildModel, GuildWebhookModel } from "index.js";
 import { env } from "@/shared/env.js";
 
 async function seed() {
-	if (!env.DEVELOPMENT_GUILD_ID || !env.DEVELOPMENT_WEBHOOK_ID || !env.DEVELOPMENT_WEBHOOK_URL)
+	if (!env.DEVELOPMENT_GUILD_ID || !env.DEVELOPMENT_WEBHOOK_ID || !env.DEVELOPMENT_WEBHOOK_URL || !env.WEBHOOK_SECRET)
 		throw new Error("Missing development environment variables");
 
 	const guildModel = new GuildModel();
@@ -11,7 +11,13 @@ async function seed() {
 
 	const guildWebhookModel = new GuildWebhookModel();
 	await guildWebhookModel.create([
-		{ guildId: env.DEVELOPMENT_GUILD_ID, id: env.DEVELOPMENT_WEBHOOK_ID, webhook: env.DEVELOPMENT_WEBHOOK_URL, type: "text" }
+		{
+			guildId: env.DEVELOPMENT_GUILD_ID,
+			id: env.DEVELOPMENT_WEBHOOK_ID,
+			webhook: env.DEVELOPMENT_WEBHOOK_URL,
+			secret: env.WEBHOOK_SECRET,
+			type: "text"
+		}
 	]);
 
 	console.info("Seeded completed successfully");
